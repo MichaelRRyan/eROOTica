b0abc8ae0fd07f4eaa666a5d43987575453d2aba
(Student) - Richard Buturla <C00272345@itcarlow.ie>
2023-02-04 12:52:25 +0000

    polished brain system

--- features/karma_system/flower_brain.gd
+++ features/karma_system/flower_brain.gd
@@ -4,7 +4,7 @@ extends Node2D
 #Constants
 var MAX_INTEREST_LEVEL = 1
 var MIN_INTEREST_LEVEL = 0
-var MAX_HEALTH_LEVEL = 0
+var MAX_HEALTH_LEVEL = 1
 var MIN_HEALTH_LEVEL = 0
 var MAX_KARMA = 1
 var MIN_KARMA = 0
@@ -17,19 +17,26 @@ enum Response {
 }
 
 #these variables will be in the range 0 - 1
-var currentFlowerInterest = MAX_INTEREST_LEVEL / 2
-var currentFlowerHealth = MAX_HEALTH_LEVEL / 2
-var currentPositiveFlowerKarma = MAX_KARMA / 2
-var currentNegativeFlowerKarma = MAX_KARMA / 2
+var currentFlowerInterest = MAX_INTEREST_LEVEL / 2.0
+var currentFlowerHealth = MAX_HEALTH_LEVEL / 2.0
+var currentPositiveFlowerKarma = MAX_KARMA / 2.0
+var currentNegativeFlowerKarma = MAX_KARMA / 2.0
 
 var interestIncrease = 0.25
 var waterHealthIncrease = 0.1
 var foodHealthIncrease = 0.3
+var positiveKarmaIncrease = 0.1
+var negativeKarmaIncrease = 0.1
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	#TESTS
+	#test_positive_response()
+	
+	#test_3_positive_responses()
+	
+	test_3_negative_responses()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -38,20 +45,74 @@ func _ready():
 
 func _on_water_received():
 	currentFlowerHealth += waterHealthIncrease
+	print("water receieved")
 	
 func _on_food_received():
 	currentFlowerHealth += foodHealthIncrease
+	print("food receieved")
 	
 func _on_dialogue_response_recieved(response):
+
+	var interestAffector = 0.05
 	match response:
 		Response.NEUTRAL_RESPONSE:
 			pass
 		Response.POSITIVE_RESPONSE:
-			currentFlowerInterest += currentPositiveFlowerKarma + currentFlowerHealth
+			interestAffector *= (currentPositiveFlowerKarma + currentFlowerHealth)
+			currentPositiveFlowerKarma += positiveKarmaIncrease
 		Response.NEGATIVE_RESPONSE:
-			currentFlowerInterest -= currentNegativeFlowerKarma 
+			interestAffector *= -(currentNegativeFlowerKarma + (1 - currentFlowerHealth))
+			currentNegativeFlowerKarma += negativeKarmaIncrease
+			
+	currentFlowerInterest += interestAffector
+	
+func test_positive_response():
+	currentFlowerHealth = 0.7
+	currentFlowerInterest = 0.5
+	currentNegativeFlowerKarma = 0.5
+	currentPositiveFlowerKarma = 0.6
+	
+	print("Before repsonse: " + str(currentFlowerInterest))
+		
+	_on_dialogue_response_recieved(Response.POSITIVE_RESPONSE)
+	
+	print("Flower Interest: " + str(currentFlowerInterest))
+	
+	
 
-
+func test_3_positive_responses():
+	
+	currentFlowerHealth = 0.5
+	currentFlowerInterest = 0.5
+	currentNegativeFlowerKarma = 0.5
+	currentPositiveFlowerKarma = 0.5
+	
+	print("Before repsonse: " + str(currentFlowerInterest))
+		
+	_on_dialogue_response_recieved(Response.POSITIVE_RESPONSE)
+	print("Flower Interest: " + str(currentFlowerInterest))
+	_on_dialogue_response_recieved(Response.POSITIVE_RESPONSE)
+	print("Flower Interest: " + str(currentFlowerInterest))
+	_on_dialogue_response_recieved(Response.POSITIVE_RESPONSE)
+	
+	print("Flower Interest: " + str(currentFlowerInterest))
+	
+func test_3_negative_responses():
+	
+	currentFlowerHealth = 0.5
+	currentFlowerInterest = 0.5
+	currentNegativeFlowerKarma = 0.5
+	currentPositiveFlowerKarma = 0.5
+	
+	print("Before repsonse: " + str(currentFlowerInterest))
+		
+	_on_dialogue_response_recieved(Response.NEGATIVE_RESPONSE)
+	print("Flower Interest: " + str(currentFlowerInterest))
+	_on_dialogue_response_recieved(Response.NEGATIVE_RESPONSE)
+	print("Flower Interest: " + str(currentFlowerInterest))
+	_on_dialogue_response_recieved(Response.NEGATIVE_RESPONSE)
+	
+	print("Flower Interest: " + str(currentFlowerInterest))
 
 
 

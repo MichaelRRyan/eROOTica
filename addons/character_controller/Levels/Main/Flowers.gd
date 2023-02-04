7fa78af2021b72c823f0786fafb7bf6122a20a33
Josh <jboycehyland15@gmail.com>
2023-02-04 14:31:22 +0000

    Update Flowers.gd

--- addons/character_controller/Levels/Main/Flowers.gd
+++ addons/character_controller/Levels/Main/Flowers.gd
@@ -1,9 +1,10 @@
 extends Node
 
-var rose:= 
+#var rose:= 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

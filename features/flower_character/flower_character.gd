522e95fe84808ac0c2da099dfd4f98fdcd3a0bef
Josh <jboycehyland15@gmail.com>
2023-02-04 14:04:16 +0000

    start of the flower, text and input working

--- features/flower_character/flower_character.gd
+++ features/flower_character/flower_character.gd
@@ -1,5 +1,10 @@
-extends Node2D
+extends Spatial
 
+
+var position : Vector3 = Vector3(1, 1, 1)
+onready var flower := get_node("FlowerSprite")
+onready var text_box:= get_node("Textbox")
+var in_range_of_player: bool = false
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -8,9 +13,28 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	position = Vector3(1, 1, 1)
+	text_box.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+
+
+func _on_Area_body_entered(body):
+	text_box.show()
+	in_range_of_player = true
+
+func _on_Area_body_exited(body):
+	text_box.hide()
+	in_range_of_player = false
+	
+	
+func _input(delta):
+	if(in_range_of_player):
+		if(Input.is_action_pressed("feed")):
+			print("You fed the flower")
+

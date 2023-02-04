d59f8605c72d81d6f4d6ed196795b12a505cef7d
Josh <jboycehyland15@gmail.com>
2023-02-04 17:48:44 +0000

    Current progress, well and can

--- addons/character_controller/Levels/Main/Flowers.gd
+++ addons/character_controller/Levels/Main/Flowers.gd
@@ -1,12 +1,31 @@
 extends Node
 
-#var rose:= 
+
+
+onready var rose:= get_node("FlowerCharacter")
+var rose_texture:= preload("res://assets/images/rose.png")
+
+onready var tulip:= get_node("FlowerCharacter2")
+var tulip_texture:= preload("res://assets/images/tulip.png")
+
+onready var nightshade:= get_node("FlowerCharacter3")
+var nightshade_texture:= preload("res://assets/images/nightshade.png")
+
+onready var poppy:= get_node("FlowerCharacter4")
+var poppy_texture:= preload("res://assets/images/poppy.png")
+
+onready var sunflower:= get_node("FlowerCharacter5")
+var sunflower_texture:= preload("res://assets/images/sunflower.png")
+
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+#func _ready():
+#	rose.set_up_flower(rose_texture)
+#	tulip.set_up_flower(tulip_texture)
+#	nightshade.set_up_flower(nightshade_texture)
+#	sunflower.set_up_flower(sunflower_texture)
+	
+	
+	
+	
+	
